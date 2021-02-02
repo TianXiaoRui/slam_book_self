450a09ef6da2cdf8cd8eb962fca48c9cbe01993b
TianXiaoRui <364368795@qq.com>
2021-02-02 17:51:56 +0800

    tr

---
+++ ch7/cmake-build-debug/CMakeFiles/feature_extraction.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/feature_extraction.dir/feature_extraction.cpp.o"
+  "CMakeFiles/feature_extraction.dir/pose_3d_3d_point.cpp.o"
+  "feature_extraction.pdb"
+  "feature_extraction"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/feature_extraction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ch7/cmake-build-debug/CMakeFiles/pose_3d3d_point.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pose_3d3d_point.dir/pose_3d_3d_point.cpp.o"
+  "pose_3d3d_point.pdb"
+  "pose_3d3d_point"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pose_3d3d_point.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
